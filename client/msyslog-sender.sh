f14e51111fbe6a488ff66235471eb825cfec8c22
Michael Grote <michael.grote@posteo.de>
2021-07-11 21:54:19 +0200

    SC2086

--- client/msyslog-sender.sh
+++ client/msyslog-sender.sh
@@ -48,7 +48,7 @@ function start_logging {
         for i in "${logfile_paths[@]}"
         do
             # start processes in background
-        	  tail --lines=0 --follow "$i" | nc -N $log_receiver_fqdn $log_receiver_port &
+        	  tail --lines=0 --follow "$i" | nc -N "$log_receiver_fqdn" "$log_receiver_port" &
             # save processid of background process
             echo $! >> "$pid_file"
         done
