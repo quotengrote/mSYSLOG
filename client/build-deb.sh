9412b0a03d15f628c40e8c649ed0c3f41d4c5e45
Michael Grote <michael.grote@posteo.de>
2021-07-12 20:19:41 +0200

    pfade

--- client/build-deb.sh
+++ client/build-deb.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-mkdir -p ./msyslog-client_$GITHUB_SHA
-mkdir -p ./msyslog-client_$GITHUB_SHA/usr/local/sbin
-mkdir -p ./msyslog-client_$GITHUB_SHA/etc
-cp ./client/msyslog-client.sh ./msyslog-client_$GITHUB_SHA/usr/local/sbin/msyslog-client.sh
-cp ./client/msyslog-client.conf ./msyslog-client_$GITHUB_SHA/etc/msyslog-client.sh
-dpkg-deb --build ./msyslog-client_$GITHUB_SHA
+mkdir -p ./msyslog-client
+mkdir -p ./msyslog-client/usr/local/sbin
+mkdir -p ./msyslog-client/etc
+cp ./client/msyslog-client.sh ./msyslog-client/usr/local/sbin/msyslog-client.sh
+cp ./client/msyslog-client.conf ./msyslog-client/etc/msyslog-client.sh
+dpkg-deb --build ./msyslog-client
