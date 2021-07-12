c4c675073255dc519e1f75c29c9307aa14401d73
Michael Grote <michael.grote@posteo.de>
2021-07-12 22:18:43 +0200

    test action

--- client/build-deb.sh
+++ client/build-deb.sh
@@ -1,19 +1,3 @@
 #!/bin/bash
-VERSION="0.0.2"
-PAKETNAME="msyslog-client"
-ORDNERNAME=
-
-sudo apt-get install dh-make devscripts
-
-
-mkdir -p $PAKETNAME-$VERSION
-cd $PAKETNAME-$VERSION
-dh_make --indep --createorig
-mv debian/rules.new debian/rules
-# Pfade Dateien
-echo msyslog-client.sh usr/local/sbin > debian/install
-echo msyslog-client.conf etc > debian/install
-
-echo "1.0" > debian/source/format
-rm debian/*.ex
-debuild -us -uc
+sudo apt-get install equivs
+equivs-build equivs-controlfile
