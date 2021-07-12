36ec83ddf7469d29fc1c17c3e7f8ba4e7b696d63
Michael Grote <michael.grote@posteo.de>
2021-07-12 13:51:37 +0200

    SC2086

--- client/msyslog-sender.sh
+++ client/msyslog-sender.sh
@@ -9,7 +9,7 @@ function set_fqdn {
     fqdn=$(hostname).$domain
 }
 function set_prefix {
-    cat $1 | sed "s/^/$i/;s/^/$fqdn\: /"
+    cat "$1" | sed "s/^/$i/;s/^/$fqdn\: /"
 }
 function get_config_from_file {
     # lese/binde ein config ein
