31f6c1e8eacc0942574e57ccbbce4442bebe6bba
Michael Grote <michael.grote@posteo.de>
2021-07-12 20:13:57 +0200

    bug

--- client/build-deb.sh
+++ client/build-deb.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-mkdir -p ./msyslog-client_$COMMIT_HASH
-mkdir -p ./msyslog-client_$COMMIT_HASH/usr/local/sbin
-mkdir -p ./msyslog-client_$COMMIT_HASH/etc
-cp ./client/msyslog-client.sh ./msyslog-client_$COMMIT_HASH/usr/local/sbin/msyslog-client.sh
-cp ./client/msyslog-client.conf ./msyslog-client_$COMMIT_HASH/etc/msyslog-client.sh
-dpkg-deb --build ./msyslog-client_$COMMIT_HASH
+mkdir -p ./msyslog-client_$GITHUB_SHA
+mkdir -p ./msyslog-client_$GITHUB_SHA/usr/local/sbin
+mkdir -p ./msyslog-client_$GITHUB_SHA/etc
+cp ./client/msyslog-client.sh ./msyslog-client_$GITHUB_SHA/usr/local/sbin/msyslog-client.sh
+cp ./client/msyslog-client.conf ./msyslog-client_$GITHUB_SHA/etc/msyslog-client.sh
+dpkg-deb --build ./msyslog-client_$GITHUB_SHA
