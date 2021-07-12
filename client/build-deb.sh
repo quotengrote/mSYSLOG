8458448e36c8ccd54585ae71e72c1c652a7f8d91
Michael Grote <michael.grote@posteo.de>
2021-07-12 22:02:38 +0200

    neuer versuch

--- client/build-deb.sh
+++ client/build-deb.sh
@@ -1,37 +1,19 @@
 #!/bin/bash
-# https://askubuntu.com/questions/27715/create-a-deb-package-from-scripts-or-binaries/27731#27731
+VERSION="0.0.2"
+PAKETNAME="msyslog-client"
+ORDNERNAME=
+
+sudo apt-get install dh-make devscripts
 
 
-# Configure your paths and filenames
-SOURCEBINPATH=client
-SOURCEBIN=msyslog-client.sh
-DEBFOLDER=client/deb
-DEBVERSION=0.1
-
-DEBFOLDERNAME=$DEBFOLDER-$DEBVERSION
-
-# Create your scripts source dir
-mkdir $DEBFOLDERNAME
-
-# Copy your script to the source dir
-cp client/msyslog-client.sh $DEBFOLDERNAME
-cp client/msyslog-client.conf $DEBFOLDERNAME
-cd $DEBFOLDERNAME
-
-# Create the packaging skeleton (debian/*)
-dh_make -s --indep --createorig
-
-# Remove make calls
-grep -v makefile debian/rules > debian/rules.new
+mkdir -p $PAKETNAME-$VERSION
+cd $PAKETNAME-$VERSION
+dh_make --indep --createorig
 mv debian/rules.new debian/rules
-
-# debian/install must contain the list of scripts to install
-# as well as the target directory
+# Pfade Dateien
 echo msyslog-client.sh usr/local/sbin > debian/install
 echo msyslog-client.conf etc > debian/install
-# Remove the example files
-rm debian/*.ex
 
-# Build the package.
-# You  will get a lot of warnings and ../somescripts_0.1-1_i386.deb
-debuild
+echo "1.0" > debian/source/format
+rm debian/*.ex
+debuild -us -uc
