d13f07cab735bd6bc1d6c47b2e5bc3a0d0a3b41f
Michael Grote <michael.grote@posteo.de>
2021-07-13 13:16:11 +0200

    pfade

--- build/build-deb-client.sh
+++ build/build-deb-client.sh
@@ -1,27 +1,28 @@
 #!/bin/bash
 # Setze Variablen
-PACKAGE_NAME=msyslog-client
+PACKAGE_NAME=msyslog-client_$GITHUB_SHA
+BUILD_DIR=$GITHUB_WORKSPACE/$PACKAGE_NAME
 
 # Installiere benötigte Pakete
 # sudo apt install dpkg -y
 
 # Erstelle Ordnerstruktur
-mkdir -p ./$PACKAGE_NAME
-mkdir -p ./$PACKAGE_NAME/DEBIAN
-mkdir -p ./$PACKAGE_NAME/etc/
-mkdir -p ./$PACKAGE_NAME/usr/local/sbin
+mkdir -p $BUILD_DIR
+mkdir -p $BUILD_DIR/DEBIAN
+mkdir -p $BUILD_DIR/etc/
+mkdir -p $BUILD_DIR/usr/local/sbin
 
 # kopiere Dateien an vorgesehe Verzeichnisse im Zielsystem
-cp ../client/dpkg-deb-controlfile ./$PACKAGE_NAME/DEBIAN/control
-cp ../client/msyslog-client.sh ./$PACKAGE_NAME/usr/local/sbin/$PACKAGE_NAME.sh
-cp ../client/msyslog-client.conf ./$PACKAGE_NAME/etc/$PACKAGE_NAME.conf
-cp ../client/dpkg-deb-postinst ./$PACKAGE_NAME/DEBIAN/postinst
+cp $GITHUB_WORKSPACE/client/dpkg-deb-controlfile $BUILD_DIR/DEBIAN/control
+cp $GITHUB_WORKSPACE/client/msyslog-client.sh $BUILD_DIR/usr/local/sbin/$PACKAGE_NAME.sh
+cp $GITHUB_WORKSPACE/client/msyslog-client.conf $BUILD_DIR/etc/$PACKAGE_NAME.conf
+cp $GITHUB_WORKSPACE/client/dpkg-deb-postinst $BUILD_DIR/DEBIAN/postinst
 
 # Setze Rechte für postinst-Script
-chmod 0755 ./$PACKAGE_NAME/DEBIAN/postinst
+chmod 0755 $BUILD_DIR/DEBIAN/postinst
 
 # Wechsel in "build"-Verzeichnis
-cd ./$PACKAGE_NAME
+cd $BUILD_DIR
 
 # baue deb
 dpkg-deb --build . $PACKAGE_NAME.deb
