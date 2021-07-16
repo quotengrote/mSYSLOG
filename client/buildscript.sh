82c94d5f91fccbfce1e464f0df9bc2c00b8ddde8
Michael Grote <38253905+quotengrote@users.noreply.github.com>
2021-07-16 20:39:51 +0200

    SC2086 (#4)

--- client/buildscript.sh
+++ client/buildscript.sh
@@ -1,38 +1,38 @@
 #!/bin/bash
 # Setze Variablen
-BUILD_DIR=$GITHUB_WORKSPACE/build
+BUILD_DIR="$GITHUB_WORKSPACE"/build
 
 # Installiere benötigte Pakete
 # sudo apt install dpkg -y
 
 # Erstelle Ordnerstruktur
-mkdir -p $BUILD_DIR
-mkdir -p $BUILD_DIR/DEBIAN
-mkdir -p $BUILD_DIR/etc/
-mkdir -p $BUILD_DIR/usr/local/sbin
-mkdir -p $BUILD_DIR/etc/systemd/system
+mkdir -p "$BUILD_DIR"
+mkdir -p "$BUILD_DIR"/DEBIAN
+mkdir -p "$BUILD_DIR"/etc/
+mkdir -p "$BUILD_DIR"/usr/local/sbin
+mkdir -p "$BUILD_DIR"/etc/systemd/system
 
 # kopiere Dateien an vorgesehe Verzeichnisse im Zielsystem
-cp $GITHUB_WORKSPACE/client/dpkg-deb-controlfile $BUILD_DIR/DEBIAN/control
-cp $GITHUB_WORKSPACE/client/msyslog-client.sh $BUILD_DIR/usr/local/sbin/msyslog-client.sh
-cp $GITHUB_WORKSPACE/client/msyslog-client.conf $BUILD_DIR/etc/msyslog-client.conf
-cp $GITHUB_WORKSPACE/client/dpkg-deb-postinst $BUILD_DIR/DEBIAN/postinst
-cp $GITHUB_WORKSPACE/client/systemd-unit $BUILD_DIR/etc/systemd/system/msyslog-client.service
+cp "$GITHUB_WORKSPACE"/client/dpkg-deb-controlfile "$BUILD_DIR"/DEBIAN/control
+cp "$GITHUB_WORKSPACE"/client/msyslog-client.sh "$BUILD_DIR"/usr/local/sbin/msyslog-client.sh
+cp "$GITHUB_WORKSPACE"/client/msyslog-client.conf "$BUILD_DIR"/etc/msyslog-client.conf
+cp "$GITHUB_WORKSPACE"/client/dpkg-deb-postinst "$BUILD_DIR"/DEBIAN/postinst
+cp "$GITHUB_WORKSPACE"/client/systemd-unit "$BUILD_DIR"/etc/systemd/system/msyslog-client.service
 
 # Setze Rechte für postinst-Script
-chmod 0755 $BUILD_DIR/DEBIAN/postinst
+chmod 0755 "$BUILD_DIR"/DEBIAN/postinst
 
 # Wechsel in "build"-Verzeichnis
-cd $BUILD_DIR
+cd "$BUILD_DIR"
 
 # baue deb
-dpkg-deb --build . msyslog-client_$GITHUB_SHA.deb
+dpkg-deb --build . msyslog-client_"$GITHUB_SHA".deb
 
 # verschiebe gebautes Paket
-mv msyslog-client_$GITHUB_SHA.deb $GITHUB_WORKSPACE/msyslog-client_$GITHUB_SHA.deb
+mv msyslog-client_"$GITHUB_SHA".deb "$GITHUB_WORKSPACE"/msyslog-client_"$GITHUB_SHA".deb
 
 ### Tests ###
-sudo apt install --fix-broken $GITHUB_WORKSPACE/msyslog-client_$GITHUB_SHA.deb -y
+sudo apt install --fix-broken "$GITHUB_WORKSPACE"/msyslog-client_"$GITHUB_SHA".deb -y
 sudo systemctl enable msyslog-client.service
 sudo systemctl start msyslog-client.service
 sudo systemctl status msyslog-client.service
