5628619504e379c707f1d0b39b7b4ecc93efc38b
Michael Grote <michael.grote@posteo.de>
2021-07-16 16:36:32 +0200

    build script

--- build/build-deb-client.sh
+++ build/build-deb-client.sh
@@ -10,12 +10,14 @@ mkdir -p $BUILD_DIR
 mkdir -p $BUILD_DIR/DEBIAN
 mkdir -p $BUILD_DIR/etc/
 mkdir -p $BUILD_DIR/usr/local/sbin
+mkdir -p $BUILD_DIR/etc/systemd/system
 
 # kopiere Dateien an vorgesehe Verzeichnisse im Zielsystem
 cp $GITHUB_WORKSPACE/client/dpkg-deb-controlfile $BUILD_DIR/DEBIAN/control
 cp $GITHUB_WORKSPACE/client/msyslog-client.sh $BUILD_DIR/usr/local/sbin/msyslog-client.sh
 cp $GITHUB_WORKSPACE/client/msyslog-client.conf $BUILD_DIR/etc/msyslog-client.conf
 cp $GITHUB_WORKSPACE/client/dpkg-deb-postinst $BUILD_DIR/DEBIAN/postinst
+cp $GITHUB_WORKSPACE/etc/systemd/system/systemd-unit $BUILD_DIR/etc/systemd/system/msyslog-client.service
 
 # Setze Rechte für postinst-Script
 chmod 0755 $BUILD_DIR/DEBIAN/postinst
