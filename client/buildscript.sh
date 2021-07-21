c9556619f08ff0f5adf766a413b97d4ac3ab4f5e
Michael Grote <michael.grote@posteo.de>
2021-07-21 12:37:10 +0200

    kommentare 2

--- client/buildscript.sh
+++ client/buildscript.sh
@@ -5,14 +5,14 @@ BUILD_DIR="$GITHUB_WORKSPACE"/build
 # Installiere benötigte Pakete
 # sudo apt install dpkg -y
 
-# Erstelle Ordnerstruktur
+echo "### Erstelle Ordnerstruktur"
 mkdir -p "$BUILD_DIR"
 mkdir -p "$BUILD_DIR"/DEBIAN
 mkdir -p "$BUILD_DIR"/etc/
 mkdir -p "$BUILD_DIR"/usr/local/sbin
 mkdir -p "$BUILD_DIR"/etc/systemd/system
 
-# kopiere Dateien an vorgesehe Verzeichnisse im Zielsystem
+echo "### kopiere Dateien an vorgesehe Verzeichnisse im Zielsystem"
 cp "$GITHUB_WORKSPACE"/client/dpkg-deb-controlfile "$BUILD_DIR"/DEBIAN/control
 cp "$GITHUB_WORKSPACE"/client/msyslog-client.sh "$BUILD_DIR"/usr/local/sbin/msyslog-client.sh
 cp "$GITHUB_WORKSPACE"/client/msyslog-client.conf "$BUILD_DIR"/etc/msyslog-client.conf
@@ -20,21 +20,21 @@ cp "$GITHUB_WORKSPACE"/client/dpkg-deb-preinst "$BUILD_DIR"/DEBIAN/preinst
 cp "$GITHUB_WORKSPACE"/client/dpkg-deb-postinst "$BUILD_DIR"/DEBIAN/postinst
 cp "$GITHUB_WORKSPACE"/client/systemd-unit "$BUILD_DIR"/etc/systemd/system/msyslog-client.service
 
-# Setze Rechte für postinst-Script
+echo "### Setze Rechte für postinst-Script"
 chmod 0755 "$BUILD_DIR"/DEBIAN/postinst
 chmod 0755 "$BUILD_DIR"/DEBIAN/preinst
 
-# Wechsel in "build"-Verzeichnis
+echo "### Wechsel in build-Verzeichnis"
 # shellcheck disable=2164
 cd "$BUILD_DIR"
 
-# baue deb
+echo "###  baue deb"
 dpkg-deb --build . msyslog-client_"$GITHUB_SHA".deb
 
-# verschiebe gebautes Paket
+echo "###  verschiebe gebautes Paket"
 mv msyslog-client_"$GITHUB_SHA".deb "$GITHUB_WORKSPACE"/msyslog-client_"$GITHUB_SHA".deb
 
-### Tests ###
+echo "###  Tests"
 sudo apt install --fix-broken "$GITHUB_WORKSPACE"/msyslog-client_"$GITHUB_SHA".deb -y
 sudo systemctl enable msyslog-client.service
 sudo systemctl start msyslog-client.service
