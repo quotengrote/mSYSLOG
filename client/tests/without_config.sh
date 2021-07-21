7f89e7c4a5ac1328faace05b0c0121c4be0b8bca
Michael Grote <michael.grote@posteo.de>
2021-07-21 15:40:35 +0200

    test ausgegliedert

---
+++ client/tests/without_config.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# Setze Variablen
+BUILD_DIR="$GITHUB_WORKSPACE"/build
+
+echo "install ncat"
+sudo apt install ncat
+
+echo "starte ncat und packe es in den hintergrund"
+sudo ncat -l -k -p 12345 &
+
+echo "Install package"
+sudo apt install --fix-broken "$GITHUB_WORKSPACE"/msyslog-client_"$GITHUB_SHA".deb -y
+
+echo "remove config"
+sudo rm /etc/msyslog-client.conf
+
+echo "restart service"
+sudo systemctl restart msyslog-client.service
+
+echo "msyslog status"
+sudo msyslog-client.sh --status
---
+++ client/tests/without_logfiles.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+# Setze Variablen
+BUILD_DIR="$GITHUB_WORKSPACE"/build
+
+echo "install ncat"
+sudo apt install ncat
+
+echo "starte ncat und packe es in den hintergrund"
+sudo ncat -l -k -p 12345 &
+
+echo "Install package"
+sudo apt install --fix-broken "$GITHUB_WORKSPACE"/msyslog-client_"$GITHUB_SHA".deb -y
+
+echo "unset logfiles"
+sudo rm /etc/msyslog-client.conf
+sudo cat <<'EOF' | sudo tee -a /etc/msyslog-client.conf
+# configfile for msyslog-client
+
+# files whose contents should be sent(comma-separated)
+logfiles=
+
+# fqdn and port to which the data should get send
+log_receiver_fqdn=127.0.0.1
+log_receiver_port=12345
+
+
+EOF
+echo "restart service"
+sudo systemctl restart msyslog-client.service
+echo "msyslog status"
+sudo systemctl status msyslog-client.service
+echo "msyslog status"
+sudo msyslog-client.sh --status
