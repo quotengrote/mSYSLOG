c01994cdf7bab59206c3322ce0f48270558bdd99
Michael Grote <michael.grote@posteo.de>
2021-07-22 09:13:46 +0200

    test in build

--- client/build.sh
+++ client/build.sh
@@ -34,3 +34,21 @@ mv msyslog-client_"$GITHUB_SHA".deb "$GITHUB_WORKSPACE"/msyslog-client_"$GITHUB_
 # Debug
 dpkg-deb --info "$GITHUB_WORKSPACE"/msyslog-client_"$GITHUB_SHA".deb
 dpkg-deb --contents "$GITHUB_WORKSPACE"/msyslog-client_"$GITHUB_SHA".deb
+
+echo "install ncat"
+sudo apt install ncat
+
+echo "starte ncat und packe es in den hintergrund"
+sudo ncat -l -k -p 12345 &
+
+echo "Install package"
+sudo apt install --fix-broken "$GITHUB_WORKSPACE"/msyslog-client_"$GITHUB_SHA".deb -y
+
+echo "remove config"
+sudo rm /etc/msyslog-client.conf
+
+echo "restart service"
+sudo systemctl restart msyslog-client.service
+
+echo "msyslog status"
+sudo /usr/local/sbin/msyslog-client.sh --status
